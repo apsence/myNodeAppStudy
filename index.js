392b0ca7dfcd1ead191c7bd0d08b08884837a429
Sasha Aerlichkon <moderor.533@gmail.com>
2026-09-05 10:49:59 +0300

    Вывод фамилии и числа П

--- index.js
+++ index.js
@@ -1,11 +1,26 @@
 const http = require('http');
 
+function calculatePi() {
+    let pi = 0;
+    
+    for (let i = 0; i < 10000; i++) {
+        pi += (i % 2 === 0 ? 1 : -1) / (2 * i + 1);
+    }
+    return (pi * 4).toFixed(2); 
+}
+
 const server = http.createServer((req, res) => {
-res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-res.end('<h1>Привет, мир!</h1>');
+    res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
+    
+    const fio = 'Грицкевич Александр Романович';
+    const group = '401 группа';
+    const piValue = calculatePi();
+
+    
+    res.end(fio + "<br>" + group + "<br>Число ПИ:" + piValue);
 });
 
 const PORT = 3000;
 server.listen(PORT, () => {
-console.log(`Сервер запущен на http://localhost:${PORT}`);
+    console.log("Сервер запущен на http://localhost:${PORT}");
 });
